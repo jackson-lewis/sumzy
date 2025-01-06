ec2b5fa7e3ed97b6e42c130b959e12a7293f2acc
Jackson Lewis <hello@jacksonlewis.co.uk>
2025-01-06 22:11:35 +0000

    jest config using babel

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -1,11 +1,5 @@
 import type { Config } from 'jest'
-import nextJest from 'next/jest.js'
- 
-const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-})
- 
+
 // Add any custom config to be passed to Jest
 const config: Config = {
   coverageProvider: 'v8',
@@ -13,9 +7,28 @@ const config: Config = {
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '^.+\\.(css|sass|scss)$': require.resolve('./__mocks__/styleMock.js'),
+    '^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp)$': require.resolve(`./__mocks__/fileMock.js`),
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '@next/font/(.*)': `<rootDir>/__mocks__/nextFontMock.js`,
+    'next/font/(.*)': `<rootDir>/__mocks__/nextFontMock.js`,
+    'server-only': `<rootDir>/__mocks__/empty.js`
+  },
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/.next/'
+  ],
+  transformIgnorePatterns: [
+    '/node_modules/(?!jose)',
+    '^.+\\.module\\.(css|sass|scss)$'
+  ],
+  watchPathIgnorePatterns: [
+    '/.next/'
+  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   }
 }
- 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config)
+
+export default config
