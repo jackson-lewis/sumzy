513ac3e27414f8a63f64a3a27b186757d0ea5938
Jackson Lewis <hello@jacksonlewis.co.uk>
2025-01-25 23:51:00 +0000

    resolve eslint errors

--- src/components/transaction/item/index.tsx
+++ src/components/transaction/item/index.tsx
@@ -1,13 +1,7 @@
 import Link from 'next/link'
 import { Transaction } from '@prisma/client'
-import { useCategories, useTx } from '@/lib/swr'
-import {
-  deleteTransaction,
-  getTransactionCategory,
-  txDirection
-} from '@/lib/transactions'
-import useTransactions from '@/lib/use-transactions'
-import Date from '@/components/global/date'
+import { useCategories } from '@/lib/swr'
+import { getTransactionCategory } from '@/lib/transactions'
 import Money from '@/components/global/money'
 import { Skeleton } from '@/components/shared/skeleton'
 import styles from './style.module.scss'
@@ -17,39 +11,12 @@ export default function TransactionItem({
 }: {
   transaction: Transaction
 }) {
-  const { showEditModal } = useTransactions()
-  const { data, mutate } = useTx()
   const { data: categories } = useCategories()
   const category = getTransactionCategory(
     transaction,
     categories?.defaultCategories,
     categories?.userCategories
   )
-
-  async function handleDeleteClick() {
-    const { data: deleteData, error } = await deleteTransaction(transaction.id)
-    console.log({ deleteData, error })
-    if (error) {
-      return console.error(error.message)
-    }
-
-    if (!deleteData || !data) {
-      return
-    }
-
-    const newTransactions = data.filter((tx) => {
-      return tx.id !== deleteData.id
-    })
-    console.log('mutating')
-
-    mutate(newTransactions, {
-      optimisticData: newTransactions
-    })
-  }
-
-  function handleUpdateClick() {
-    showEditModal(txDirection(transaction), transaction.frequency, transaction)
-  }
 
   return (
     <div className={styles.transaction}>
